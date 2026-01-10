208d47803f644ca6fcad96d9370e234e1a86c2e9
Ahmad Haikal <haikala0010@gmail.com>
2026-01-10 17:22:02 +0700

    feat: implement course management API with CRUD operations and update environment configuration

--- course-service/src/app.js
+++ course-service/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const courseRoutes = require('./routes/course.route');
 
 const app = express();
 app.use(express.json());
@@ -7,6 +8,8 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
+
+app.use('/api/courses', courseRoutes);
 
 app.get('/health', (req, res) => {
     res.json({ status: 'Course Service is Running'});
