656d2a9933afb4350859a78bbd65a6c8d9d27186
RestiAdelia <adeliaresti101@gmail.com>
2026-01-15 23:37:02 +0700

    Mengambil id pada user yang login dan get all enrollment

--- enrollment-service/app/routers/enrollment.py
+++ enrollment-service/app/routers/enrollment.py
@@ -46,3 +46,9 @@ def get_enrollments_by_user(user_id: str, db: Session = Depends(get_db)):
         .all()
     )
     return enrollments
+
+@router.get("", response_model=List[EnrollmentResponse])
+def get_all_enrollments(db: Session = Depends(get_db)):
+    # Mengambil SEMUA data dari tabel enrollments
+    enrollments = db.query(Enrollment).all()
+    return enrollments
